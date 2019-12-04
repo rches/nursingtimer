264439d2e3779f0308f3366a8a2e59bb41228241
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-12-03 19:55:15 -0500

    adding event handler for button component

--- src/components/button.js
+++ src/components/button.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 class Button extends React.Component {
 
+    
+         handleClick = (e) => {
+            e.preventDefault();
+            console.log(`You have a new date! ${new Date()}`)
+        }
+    
+
     render() {
         return(
-            <button>
+            <button onClick={this.handleClick}>
                 Click me
             </button>
         )
