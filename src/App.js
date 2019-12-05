a59a5068bd5ad7195ced06534a29437f3980b281
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-12-04 21:49:38 -0500

    removing button component to move into app for better understanding of state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,39 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
-import Button from './components/button';
 import TextArea from './components/textarea';
 
 // import { directive } from '@babel/types';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newDate: '',
+      isOn: false,
+      clickCount: 0
+
+    }
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.setState({isOn: !this.state.isOn});
+    if(this.state.isOn) {
+      this.setState({newDate: new Date()})
+    }       
+    
+  }
+
   render () {
     const text = "this is some test text"
     return(
       <div className="wrapper">
       <Header />
-      <Button />
-      <TextArea text={text}/>
+      <button onClick={this.handleClick}>
+                {this.state.isOn? 'Stop the time' : 'Start the time'}
+            </button>
+      <TextArea text={this.state.newDate.toString()}/>
       <Footer />
       </div>
     )
