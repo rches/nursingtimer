f31ef5bb059b808b54d0f9390b2b6123d7c0b190
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-12-04 22:20:52 -0500

    linted code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-import React from 'react';
-import './App.css';
-import Header from './components/header';
-import Footer from './components/footer';
-import TextArea from './components/textarea';
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import TextArea from "./components/textarea";
 
 // import { directive } from '@babel/types';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      startDate: '',
-      endDate: '',
-      isOn: false,
-      clickCount: 0
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: "",
+            endDate: "",
+            isOn: false,
+            clickCount: 0
+        };
     }
-  }
 
-  handleClick = (e) => {
-    e.preventDefault();
-    this.setState({isOn: !this.state.isOn});
-    this.setState({clickCount: +1})
-    if(!this.state.isOn) {
-      this.setState({startDate: new Date()})
-    } else if (this.state.isOn)        {
+    handleClick = e => {
+        e.preventDefault();
+        this.setState({ isOn: !this.state.isOn });
+        this.setState({ clickCount: +1 });
+        if (!this.state.isOn) {
+            this.setState({ startDate: new Date() });
+        } else if (this.state.isOn) {
+        }
+    };
 
+    render() {
+        return (
+            <div className="wrapper">
+                <Header />
+                <button onClick={this.handleClick}>
+                    {this.state.isOn ? "Stop the time" : "Start the time"}
+                </button>
+                <TextArea text={this.state.startDate.toString()} />
+                <Footer />
+            </div>
+        );
     }
-    
-  }
-
-  render () {
-    return(
-      <div className="wrapper">
-      <Header />
-      <button onClick={this.handleClick}>
-                {this.state.isOn? 'Stop the time' : 'Start the time'}
-            </button>
-      <TextArea text={this.state.startDate.toString()}/>
-      <Footer />
-      </div>
-    )
-  }
 }
 
 export default App;
